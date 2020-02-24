d96e96ae01a51ccad0458b6fa3228858bab627f0
shawnx <shawn.xu@nokia-sbell.com>
2020-02-24 13:22:42 +0800

    modify hello.py for the 2rd trigger

--- hello.py
+++ hello.py
@@ -2,3 +2,4 @@
 
 print('This is test for jenkins - branch dev')
 print('This is test for jenkins - -auto-trigger - branch dev')
+print('This is test for jenkins - -auto-trigger - 2rd - branch dev')
